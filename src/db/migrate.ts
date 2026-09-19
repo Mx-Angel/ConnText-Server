0a92ed9c1efbaed830ae2b6e4a1854a96e14e30b
Mx-Angel <149399937+Mx-Angel@users.noreply.github.com>
2026-09-19 11:57:38 +0100

    Remove debug code + update format and linting rules
    - Remove console.log calls in migrate.ts
    - Remove deprecated function use in eslint config
    - Change line width to 100
    
    Signed-off-by: Mx-Angel <149399937+Mx-Angel@users.noreply.github.com>

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -5,23 +5,8 @@ const client = await connectionPool.connect();
 
 async function migrateDB(): Promise<void> {
   try {
-    const connectionInfo = await client.query(`
-      SELECT
-      current_database() AS database,
-      current_user AS user,
-      current_schema() AS schema,
-      inet_server_addr() AS host,
-      inet_server_port() AS port;
-    `);
-    console.log(connectionInfo.rows[0]);
-    const serverDetailsScript = fs.readFileSync(
-      'src/db/migrations/001_server_details_schema.sql',
-      'utf-8',
-    );
-    const communcationScript = fs.readFileSync(
-      'src/db/migrations/002_communication_schema.sql',
-      'utf-8',
-    );
+    const serverDetailsScript = fs.readFileSync('src/db/migrations/001_server_details_schema.sql', 'utf-8');
+    const communcationScript = fs.readFileSync('src/db/migrations/002_communication_schema.sql', 'utf-8');
     const auditScript = fs.readFileSync('src/db/migrations/003_audit_schema.sql', 'utf-8');
 
     await client.query('BEGIN');
@@ -29,12 +14,9 @@ async function migrateDB(): Promise<void> {
     await client.query(communcationScript);
     await client.query(auditScript);
     await client.query('COMMIT');
-
-    console.log('Migration successful');
   } catch (error) {
     // Add logger
     await client.query('ROLLBACK');
-    console.log(`An error occured whilst trying to read .sql files: ${error}`);
   } finally {
     client.release();
     await connectionPool.end();
